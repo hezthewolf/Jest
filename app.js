6e9018bf031c70a60de97ab23f909ac973b20705
Hez <mokuahesbon254@gmail.com>
2021-12-02 09:40:02 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const people = require('./route/people')
+const login = require('./route/auth')
+
+// Static assets
+
+app.use(express.static('./methods-public'))
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+app.use('/api/people', people)
+app.use('/login', login)
+
+
+
+
+app.listen(5000, () => {
+    console.log("Server is listening on port 5000...");
+})
